a4db317c5178ed7e5eb1122aa725a13e8fd890e7
102051563 <102051563@TD224-012>
2019-06-18 18:38:00 +1000

    layout done, functions needed

---
+++ EDSLPrototype/EDSLPrototype/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace EDSLPrototype
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string filepath = @"C:\Users\102051563\source\repos\EDSLPrototype\Files\data.txt";
+        public List<int> roundList { get; set; }
+        public List<string> datList { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void displayResultsScreen(object sender, RoutedEventArgs e)
+        {
+            var ResultsScreen = new Results();
+            ResultsScreen.Show();
+            Close();
+        }
+
+        public void generateDates(object sender, RoutedEventArgs e)
+        {
+            
+            dateList.Items.Add(startDateDropBox.Text);
+            dateList.Items.Add("15/06/2019");
+        }
+
+    }
+}
---
+++ EDSLPrototype/EDSLPrototype/Season.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDSLPrototype
+{
+    class Season
+    {
+        public string Name { get; set; }
+        public int noOfRounds { get; set; }
+        public DateTime startDate { get; set; }
+        public List<DateTime> dateList { get; set; }
+        public List<int> roundList { get; set; }
+        
+        public Season(string name, int roundNo, DateTime start, List<DateTime> dates, List<int> rounds)
+        {
+            name = Name;
+            roundNo = noOfRounds;
+            start = startDate;
+            dates = dateList;
+            rounds = roundList;
+        }
+
+        public List<DateTime> populateRoundDates(DateTime startDate)
+        {
+            //grab the current date and make a number of rounds starting from that date
+            List<DateTime> dates = new List<DateTime>();
+            dates.Add(startDate);
+            for (int i = 0; i < noOfRounds - 1; i++)
+            {
+                DateTime date = GetNextSaturday(DateTime.Today.AddDays(1), DayOfWeek.Saturday);
+                dates.Add(date);
+            }
+            return dates;
+        }
+
+        public List<int> populateRounds(int noOfRounds)
+        {
+            List<int> rounds = new List<int>();
+            return rounds;
+            //create the round numbers for the table
+            
+        }
+
+        public static DateTime GetNextSaturday(DateTime start, DayOfWeek day)
+        {
+            
+            int daysToAdd = ((int)day - (int)start.DayOfWeek + 7) % 7;
+            return start.AddDays(daysToAdd);
+        }
+    }
+}
